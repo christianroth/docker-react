81dc4f1a30c8f2023640f88a9cc7ae0a6043abfa
christianroth <croth@splunk.com>
2020-05-25 11:12:34 -0700

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Yo! Yoooooooooooooooooo! Edit <code>src/App.js</code> and save to reload.
+          Yo! Yoooooooooooooooooo! UPDATED <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
